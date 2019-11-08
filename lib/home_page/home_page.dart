4b20b2ea060bee6b9cb7f6bde139cdc60c5b1336
parag <parag@foore.in>
2019-11-08 16:46:21 +0530

    minor fix

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -16,9 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
+  final List<Widget> _widgetOptions = <Widget>[
     PeoplePage(),
     ReviewPage(),
   ];
