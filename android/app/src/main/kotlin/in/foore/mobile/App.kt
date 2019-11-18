840b3d0f70f76afa731371671152d741c2a76fe6
parag <parag@foore.in>
2019-11-18 19:33:13 +0530

    branch integration

--- android/app/src/main/kotlin/in/foore/mobile/App.kt
+++ android/app/src/main/kotlin/in/foore/mobile/App.kt
@@ -4,11 +4,24 @@ import io.flutter.app.FlutterApplication
 import android.content.Context
 import androidx.multidex.MultiDex
 
+import io.branch.referral.Branch
+
+
 class App : FlutterApplication() {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Branch logging for debugging
+        Branch.enableDebugMode()
+
+        // Branch object initialization
+        Branch.getAutoInstance(this)
     }
 
 }
--- android/app/src/main/kotlin/in/foore/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/in/foore/mobile/MainActivity.kt
@@ -4,10 +4,35 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.branch.referral.Branch
+import io.branch.referral.BranchError
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
   }
+
+  override fun onStart() {
+    super.onStart()
+
+    // Branch init
+    Branch.getInstance().initSession(object : BranchReferralInitListener {
+      override fun onInitFinished(referringParams: JSONObject, error: BranchError?) {
+        if (error == null) {
+          Log.e("BRANCH SDK", referringParams.toString())
+          // Retrieve deeplink keys from 'referringParams' and evaluate the values to determine where to route the user
+          // Check '+clicked_branch_link' before deciding whether to use your Branch routing logic
+        } else {
+          Log.e("BRANCH SDK", error.message)
+        }
+      }
+    }, this.intent.data, this)
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    this.intent = intent
+  }
 }
+
