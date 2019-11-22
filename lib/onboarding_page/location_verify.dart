9cb512e75577a6085468c80ba983f76b30a49d03
parag <parag@foore.in>
2019-11-22 14:06:41 +0530

    Added verify with pin form

--- lib/onboarding_page/location_verify.dart
+++ lib/onboarding_page/location_verify.dart
@@ -36,6 +36,7 @@ class LocationVerifyPage extends StatefulWidget {
 class _LocationVerifyPageState extends State<LocationVerifyPage>
     with AfterLayoutMixin<LocationVerifyPage> {
   Completer<GoogleMapController> _controller = Completer();
+  final _formVerifyWithPin = GlobalKey<FormState>();
   @override
   void afterFirstLayout(BuildContext context) {
     final completeVerificationBloc =
@@ -48,7 +49,9 @@ class _LocationVerifyPageState extends State<LocationVerifyPage>
     final completeVerificationBloc =
         Provider.of<CompleteVerificationBloc>(context);
     onVerify() {
-      completeVerificationBloc.completeVerification(context);
+      if (_formVerifyWithPin.currentState.validate()) {
+        completeVerificationBloc.completeVerification(context);
+      }
     }
 
     return Scaffold(
@@ -74,65 +77,67 @@ class _LocationVerifyPageState extends State<LocationVerifyPage>
     );
   }
 
-  Column verificationWidget(
+  Widget verificationWidget(
       BuildContext context, GmbLocationItem location, Function onVerify) {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: 32.0,
-        ),
-        Container(
-          child: ListTile(
-            title: Text(location?.locationName ?? ''),
-            subtitle: Text(
-                '# 51, Shanti Nivas 7th Cross chinapanahalli, "Doddanekundi, Ext, Bengaluru, Karnataka'),
+    final completeVerificationBloc =
+        Provider.of<CompleteVerificationBloc>(context);
+    return Form(
+      key: this._formVerifyWithPin,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 32.0,
           ),
-        ),
-        Divider(
-          thickness: 1.0,
-          indent: 16.0,
-          endIndent: 16.0,
-          height: 32.0,
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 12.0,
-          ),
-          child: Text(
-            'Enter the 5-digit verification code mailed to this address on Nov 21, 2019. Postcards take about 12 days to arrive.',
-            style: Theme.of(context).textTheme.subtitle,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 16.0,
-          ),
-          child: TextFormField(
-            // controller: this._loginBloc.otpEditController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Enter code",
+          Container(
+            child: ListTile(
+              title: Text(location?.locationName ?? ''),
+              subtitle: Text(
+                  '# 51, Shanti Nivas 7th Cross chinapanahalli, "Doddanekundi, Ext, Bengaluru, Karnataka'),
             ),
-            maxLength: 5,
-            // validator: (String value) {
-            //   return value.length < 1
-            //       ? AppTranslations.of(context)
-            //           .text("otp_page_enter_otp_validation")
-            //       : null;
-            // },
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-          width: double.infinity,
-          child: RaisedButton(
-            onPressed: onVerify,
-            child: Text('Verify'),
+          Divider(
+            thickness: 1.0,
+            indent: 16.0,
+            endIndent: 16.0,
+            height: 32.0,
           ),
-        )
-      ],
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 12.0,
+            ),
+            child: Text(
+              'Enter the 5-digit verification code mailed to this address on Nov 21, 2019. Postcards take about 12 days to arrive.',
+              style: Theme.of(context).textTheme.subtitle,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+            ),
+            child: TextFormField(
+              controller: completeVerificationBloc.pinEditController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Enter code",
+              ),
+              maxLength: 5,
+              validator: (String value) {
+                return value.length < 5 ? 'Code must be 5 of digits' : null;
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            width: double.infinity,
+            child: RaisedButton(
+              onPressed: onVerify,
+              child: Text('Verify'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
