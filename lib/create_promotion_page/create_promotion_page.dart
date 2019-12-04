341327585df431042c708a85a5f36ce48692cca5
parag <parag@foore.in>
2019-12-04 15:20:21 +0530

    added map

--- lib/create_promotion_page/create_promotion_page.dart
+++ lib/create_promotion_page/create_promotion_page.dart
@@ -25,155 +25,204 @@ class _CreatePromotionPageState extends State<CreatePromotionPage> {
   @override
   Widget build(BuildContext context) {
     final onboardingGuardBloc = Provider.of<OnboardingGuardBloc>(context);
-    return Stack(
-      children: <Widget>[
-        GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(
-              12.9829735,
-              77.687969,
-            ),
-            zoom: 14.4746,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Promotion'),
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.transparent,
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 12,
           ),
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-        ),
-        Scaffold(
-          appBar: AppBar(
-            title: Text('Create Promotion'),
-            automaticallyImplyLeading: true,
-            backgroundColor: Colors.transparent,
-          ),
-          backgroundColor: Colors.white,
-          body: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 12,
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                width: double.infinity,
-                child: StreamBuilder<OnboardingGuardState>(
-                    stream: onboardingGuardBloc.onboardingStateObservable,
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return Container();
-                      }
-                      return DropdownButton<FoLocations>(
-                        value: snapshot.data.locations[0],
-                        elevation: 4,
-                        onChanged: (value) {},
-                        items: snapshot.data.locations
-                            .map<DropdownMenuItem<FoLocations>>(
-                                (FoLocations locationItem) {
-                          return DropdownMenuItem<FoLocations>(
-                            value: locationItem,
-                            child: Text(locationItem.name),
-                          );
-                        }).toList(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            width: double.infinity,
+            child: StreamBuilder<OnboardingGuardState>(
+                stream: onboardingGuardBloc.onboardingStateObservable,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Container();
+                  }
+                  var value = null;
+                  if (snapshot.data.locations.length > 0) {
+                    value = snapshot.data.locations[0];
+                  }
+                  return DropdownButton<FoLocations>(
+                    value: value,
+                    elevation: 4,
+                    onChanged: (value) {},
+                    items: snapshot.data.locations
+                        .map<DropdownMenuItem<FoLocations>>(
+                            (FoLocations locationItem) {
+                      return DropdownMenuItem<FoLocations>(
+                        value: locationItem,
+                        child: Text(locationItem.name),
                       );
-                    }),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                width: double.infinity,
-                child: Text(
-                  'Send messages to nearby customers',
-                  style: Theme.of(context).textTheme.subtitle,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  minLines: 3,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                    labelText: 'Type your promotional message',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 32.0,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Text(
-                  'You can reach upto 4985 people near your store.',
-                  style: Theme.of(context).textTheme.title.copyWith(),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-                color: Color.fromRGBO(4, 196, 204, 1),
-                padding: EdgeInsets.symmetric(
-                  vertical: 15.0,
-                  horizontal: 30,
-                ),
-                child: Container(
-                    width: MediaQuery.of(context).size.width * 0.6,
-                    child: Text(
-                      'Rs. 100-1012 People',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
-                    )),
-                onPressed: () {},
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              RaisedButton(
-                color: Color.fromRGBO(4, 150, 204, 1),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-                padding: EdgeInsets.symmetric(
-                  vertical: 15.0,
-                  horizontal: 30,
-                ),
-                child: Container(
-                    width: MediaQuery.of(context).size.width * 0.6,
-                    child: Text(
-                      'Rs. 100-1012 People',
-                      textAlign: TextAlign.center,
-                    )),
-                onPressed: () {},
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              RaisedButton(
-                color: Color.fromRGBO(4, 86, 91, 1),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-                padding: EdgeInsets.symmetric(
-                  vertical: 15.0,
-                  horizontal: 30,
-                ),
-                child: Container(
-                    width: MediaQuery.of(context).size.width * 0.6,
-                    child: Text(
-                      'Rs. 100-1012 People',
-                      textAlign: TextAlign.center,
-                    )),
-                onPressed: () {},
-              ),
-            ],
+                    }).toList(),
+                  );
+                }),
           ),
-        ),
-      ],
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            width: double.infinity,
+            child: Text(
+              'Send messages to nearby customers',
+              style: Theme.of(context).textTheme.subtitle,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              minLines: 3,
+              maxLines: 3,
+              decoration: InputDecoration(
+                labelText: 'Type your promotional message',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Material(
+              elevation: 2,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    child: GoogleMap(
+                      mapType: MapType.normal,
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(
+                          12.9829735,
+                          77.687969,
+                        ),
+                        zoom: 14.4746,
+                      ),
+                      onMapCreated: (GoogleMapController controller) {
+                        _controller.complete(controller);
+                      },
+                      circles: Set.from([
+                        Circle(
+                          circleId: CircleId('foCircle'),
+                          center: LatLng(
+                            12.9829735,
+                            77.687969,
+                          ),
+                          radius: 50,
+                          fillColor: Colors.blue,
+                          strokeWidth: 0,
+                        ),
+                        Circle(
+                          circleId: CircleId('foCircle2'),
+                          center: LatLng(
+                            12.9829735,
+                            77.687969,
+                          ),
+                          radius: 240,
+                          fillColor: Colors.blue[100].withOpacity(0.12),
+                          strokeWidth: 6,
+                          strokeColor: Colors.blue[100],
+                        ),
+                      ]),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 12,
+                    ),
+                    color: Colors.green[100],
+                    child: Text(
+                      'You can reach upto 4985 people near your store.',
+                      style: Theme.of(context).textTheme.body1.copyWith(
+                            color: Colors.green[700],
+                          ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              color: Color.fromRGBO(4, 196, 204, 1),
+              padding: EdgeInsets.symmetric(
+                vertical: 15.0,
+                horizontal: 30,
+              ),
+              child: Text(
+                'Rs. 100-1012 People',
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .button
+                    .copyWith(color: Colors.white),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: RaisedButton(
+              color: Color.fromRGBO(4, 150, 204, 1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 15.0,
+                horizontal: 30,
+              ),
+              child: Text(
+                'Rs. 100-1012 People',
+                textAlign: TextAlign.center,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: RaisedButton(
+              color: Color.fromRGBO(4, 86, 91, 1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 15.0,
+                horizontal: 30,
+              ),
+              child: Text(
+                'Rs. 100-1012 People',
+                textAlign: TextAlign.center,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
