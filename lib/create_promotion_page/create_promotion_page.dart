30d5eb62933f5ba8e3b097957496ff252f99ab6a
parag <parag@foore.in>
2019-12-05 22:48:55 +0530

    fixed langues

--- lib/create_promotion_page/create_promotion_page.dart
+++ lib/create_promotion_page/create_promotion_page.dart
@@ -217,7 +217,7 @@ class _CreatePromotionPageState extends State<CreatePromotionPage>
               return createPromotion(context);
             } else if (snapshot.data.screenType ==
                 CreatePromotionScreens.promotionSent) {
-              // return createPromotion(context);
+              return createPromotion(context);
               return listPromotion(context);
             }
             return Container();
