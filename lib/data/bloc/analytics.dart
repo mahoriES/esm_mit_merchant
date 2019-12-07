fb0707e50641b8bc57cf1da80797a403a46be3e3
parag <parag@foore.in>
2019-12-07 14:38:21 +0530

    events added

---
+++ lib/data/bloc/analytics.dart
@@ -0,0 +1,121 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/material.dart';
+import 'package:foore/data/model/login.dart';
+import 'package:foore/environments/environment.dart';
+import 'package:intercom_flutter/intercom_flutter.dart';
+
+class FoAnalytics {
+  FirebaseAnalytics _firebaseAnalytics;
+
+  bool isInitialized = false;
+  bool isUserIdentified = false;
+
+  init() async {
+    await intercomInit();
+    firebaseAnalyticsInit();
+    this.isInitialized = true;
+  }
+
+  identifyUser(AuthInfo authData) {
+    intercomLogin(authData);
+    firebaseAnalyticsLogin(authData);
+    isUserIdentified = true;
+  }
+
+  resetUserIdentity() {
+    this.intercomLogout();
+    this.firebaseAnalyticsLogout();
+    isUserIdentified = false;
+  }
+
+  intercomInit() async {
+    await Intercom.initialize(
+      Environment.intercomAppId,
+      iosApiKey: Environment.intercomIosApiKey,
+      androidApiKey: Environment.intercomAndroidApiKey,
+    );
+  }
+
+  firebaseAnalyticsInit() {
+    this._firebaseAnalytics = FirebaseAnalytics();
+    _firebaseAnalytics.setAnalyticsCollectionEnabled(Environment.isProd);
+  }
+
+  intercomLogin(AuthInfo authData) async {
+    await Intercom.initialize(
+      Environment.intercomAppId,
+      iosApiKey: Environment.intercomIosApiKey,
+      androidApiKey: Environment.intercomAndroidApiKey,
+    );
+    await Intercom.registerIdentifiedUser(
+        userId: authData.userProfile.userUuid);
+    await Intercom.updateUser(
+      email: authData.userProfile.email,
+      name: authData.userProfile.name,
+      company: authData.companyInfo.name,
+      companyId: authData.companyInfo.companyUuid,
+    );
+  }
+
+  firebaseAnalyticsLogin(AuthInfo authData) {
+    this._firebaseAnalytics.setUserId(authData.userProfile.userUuid);
+    this
+        ._firebaseAnalytics
+        .setUserProperty(name: 'email', value: authData.userProfile.email);
+    this
+        ._firebaseAnalytics
+        .setUserProperty(name: 'name', value: authData.userProfile.name);
+    this
+        ._firebaseAnalytics
+        .setUserProperty(name: 'company', value: authData.companyInfo.name);
+    this._firebaseAnalytics.setUserProperty(
+        name: 'companyId', value: authData.companyInfo.companyUuid);
+  }
+
+  intercomLogout() async {
+    await Intercom.logout();
+  }
+
+  firebaseAnalyticsLogout() {
+    this._firebaseAnalytics.resetAnalyticsData();
+  }
+
+  trackUserEvent({@required String name, Map<String, dynamic> parameters}) {
+    if (isUserIdentified) {
+      Intercom.logEvent(name, parameters);
+      this._firebaseAnalytics.logEvent(name: name, parameters: parameters);
+    }
+  }
+
+  setCurrentScreen(String screenName) {
+    this._firebaseAnalytics.setCurrentScreen(screenName: screenName);
+  }
+
+  addUserProperties({@required String name, @required String value}) {
+    if (isUserIdentified) {
+      Map customAttributes = new Map();
+      customAttributes[name] = value;
+      Intercom.updateUser(customAttributes: customAttributes);
+
+      this._firebaseAnalytics.setUserProperty(name: name, value: value);
+    }
+  }
+}
+
+
+class FoAnalyticsEvents {
+  static const checkin_by_clicking_on_contact = 'checkin by clicking on contact';
+  static const checkin_by_manual_entry_of_name_num = 'checkin by manual entry of name num';
+  static const bulk_checkin = 'bulk checkin';
+  static const app_shared = 'app shared';
+  static const nearby_promo_clicked = 'nearby promo clicked';
+  static const nearby_Promo_created = 'nearby Promo created';
+}
+
+class FoAnalyticsUserProperties {
+  static const no_of_locations = 'no of locations';
+  static const language_chosen = 'language chosen';
+  static const google_locations_exists_or_not = 'google locations exists or not';
+  static const google_locations_categories = 'google locations categories';
+
+}
--- lib/data/bloc/auth.dart
+++ lib/data/bloc/auth.dart
@@ -1,15 +1,13 @@
 import 'dart:convert';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flutter/material.dart';
 import 'package:foore/data/bloc/push_notifications.dart';
 import 'package:foore/data/branch_service.dart';
 import 'package:foore/data/http_service.dart';
 import 'package:foore/data/model/login.dart';
-import 'package:foore/environments/environment.dart';
-import 'package:intercom_flutter/intercom_flutter.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+import 'analytics.dart';
 
 enum AuthType { Google, Foore }
 
@@ -184,103 +182,5 @@ class AuthState {
 
   AuthState() {
     this.isLoading = false;
-  }
-}
-
-class FoAnalytics {
-  FirebaseAnalytics _firebaseAnalytics;
-
-  bool isInitialized = false;
-  bool isUserIdentified = false;
-
-  init() async {
-    await intercomInit();
-    firebaseAnalyticsInit();
-    this.isInitialized = true;
-  }
-
-  identifyUser(AuthInfo authData) {
-    intercomLogin(authData);
-    firebaseAnalyticsLogin(authData);
-    isUserIdentified = true;
-  }
-
-  resetUserIdentity() {
-    this.intercomLogout();
-    this.firebaseAnalyticsLogout();
-    isUserIdentified = false;
-  }
-
-  intercomInit() async {
-    await Intercom.initialize(
-      Environment.intercomAppId,
-      iosApiKey: Environment.intercomIosApiKey,
-      androidApiKey: Environment.intercomAndroidApiKey,
-    );
-  }
-
-  firebaseAnalyticsInit() {
-    this._firebaseAnalytics = FirebaseAnalytics();
-    _firebaseAnalytics.setAnalyticsCollectionEnabled(Environment.isProd);
-  }
-
-  intercomLogin(AuthInfo authData) async {
-    await Intercom.initialize(
-      Environment.intercomAppId,
-      iosApiKey: Environment.intercomIosApiKey,
-      androidApiKey: Environment.intercomAndroidApiKey,
-    );
-    await Intercom.registerIdentifiedUser(
-        userId: authData.userProfile.userUuid);
-    await Intercom.updateUser(
-      email: authData.userProfile.email,
-      name: authData.userProfile.name,
-      company: authData.companyInfo.name,
-      companyId: authData.companyInfo.companyUuid,
-    );
-  }
-
-  firebaseAnalyticsLogin(AuthInfo authData) {
-    this._firebaseAnalytics.setUserId(authData.userProfile.userUuid);
-    this
-        ._firebaseAnalytics
-        .setUserProperty(name: 'email', value: authData.userProfile.email);
-    this
-        ._firebaseAnalytics
-        .setUserProperty(name: 'name', value: authData.userProfile.name);
-    this
-        ._firebaseAnalytics
-        .setUserProperty(name: 'company', value: authData.companyInfo.name);
-    this._firebaseAnalytics.setUserProperty(
-        name: 'companyId', value: authData.companyInfo.companyUuid);
-  }
-
-  intercomLogout() async {
-    await Intercom.logout();
-  }
-
-  firebaseAnalyticsLogout() {
-    this._firebaseAnalytics.resetAnalyticsData();
-  }
-
-  trackUserEvent({@required String name, Map<String, dynamic> parameters}) {
-    if (isUserIdentified) {
-      Intercom.logEvent(name, parameters);
-      this._firebaseAnalytics.logEvent(name: name, parameters: parameters);
-    }
-  }
-
-  setCurrentScreen(String screenName) {
-    this._firebaseAnalytics.setCurrentScreen(screenName: screenName);
-  }
-
-  addUserProperties({@required String name, @required String value}) {
-    if (isUserIdentified) {
-      Map customAttributes = new Map();
-      customAttributes[name] = value;
-      Intercom.updateUser(customAttributes: customAttributes);
-
-      this._firebaseAnalytics.setUserProperty(name: name, value: value);
-    }
   }
 }
