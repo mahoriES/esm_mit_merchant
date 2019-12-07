580a30c8ee4fa8ae8a4d0f9b9bb24e04bec39f4e
parag <parag@foore.in>
2019-12-07 16:47:45 +0530

    fixed

--- lib/data/bloc/analytics.dart
+++ lib/data/bloc/analytics.dart
@@ -88,43 +88,56 @@ class FoAnalytics {
   }
 
   setCurrentScreen(String screenName) {
-    this._firebaseAnalytics.setCurrentScreen(screenName: screenName);
+    // try {
+    //   this._firebaseAnalytics.setCurrentScreen(screenName: screenName);
+    // } catch (err) {}
   }
 
   addUserProperties({@required String name, @required dynamic value}) {
-    if (isUserIdentified) {
-      Map customAttributes = new Map();
-      customAttributes[name] = value;
-      Intercom.updateUser(customAttributes: customAttributes);
-
-      this._firebaseAnalytics.setUserProperty(name: name, value: value.toString());
+    print(name);
+    print(value.toString());
+    try {
+      if (isUserIdentified) {
+        final customAttributes = new Map<String, dynamic>();
+        customAttributes[name] = value;
+        Intercom.updateUser(customAttributes: customAttributes);
+        this
+            ._firebaseAnalytics
+            .setUserProperty(name: name, value: value.toString());
+      }
+    } catch (err, stacktrace) {
+      print(err.toString());
+      print(stacktrace.toString());
     }
   }
 }
 
-
 class FoAnalyticsEvents {
-  static const checkin_by_clicking_on_contact = 'checkin by clicking on contact';
-  static const checkin_by_manual_entry_of_name_num = 'checkin by manual entry of name num';
-  static const bulk_checkin = 'bulk checkin';
-  static const app_shared = 'app shared';
-  static const nearby_promo_clicked = 'nearby promo clicked';
-  static const nearby_promo_created = 'nearby promo created';
+  static const checkin_by_clicking_on_contact =
+      'checkin_by_clicking_on_contact';
+  static const checkin_by_manual_entry_of_name_num =
+      'checkin_by_manual_entry_of_name_num';
+  static const bulk_checkin = 'bulk_checkin';
+  static const app_shared = 'app_shared';
+  static const nearby_promo_clicked = 'nearby_promo_clicked';
+  static const nearby_promo_created = 'nearby_promo_created';
 }
 
 class FoAnalyticsUserProperties {
-  static const no_of_locations = 'no of locations';
-  static const language_chosen = 'language chosen';
-  static const google_locations_count = 'google locations count';
+  static const no_of_locations = 'no_of_locations';
+  static const language_chosen = 'language_chosen';
+  static const google_locations_count = 'google_locations_count';
   static const google_locations_info = 'google_locations_info';
-  static const google_location_created_from_app = 'google_location_created_from_app';
-  static const google_location_verification_started_from_app = 'google_location_verification_started_from_app';
-  static const google_location_verification_done_from_app = 'google_location_verification_done_from_app';
-  static const nearby_promo_created = 'nearby promo created';
+  static const google_location_created_from_app =
+      'google_location_created_from_app';
+  static const google_location_verification_started_from_app =
+      'google_location_verification_started_from_app';
+  static const google_location_verification_done_from_app =
+      'google_location_verification_done_from_app';
+  static const nearby_promo_created = 'nearby_promo_created';
   static const uses_google_to_login = 'uses_google_to_login';
   static const uses_company_email_to_login = 'uses_company_email_to_login';
   static const no_of_feedbacks = 'no_of_feedbacks';
   static const no_of_unirson = 'no_of_unirson';
-  static const app_shared = 'app shared';
-
+  static const app_shared = 'app_shared';
 }
