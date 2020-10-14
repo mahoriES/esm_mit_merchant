21aea23c347d5dc240cd2a97e13cfc7ac295712a
Kaustubh Pandey <kaustubhkpintel@gmail.com>
2020-10-14 11:37:48 +0530

    Set the minimum deployment target for iOS to 9.0 and added doc comments for the image cropper class

--- lib/widgets/image_cropper.dart
+++ lib/widgets/image_cropper.dart
@@ -5,45 +5,64 @@ import 'package:foore/app_colors.dart';
 ///The [ImageCropperView] class uses the plugin to show two native UI components
 ///(based on uCrop and TOCropViewController)
 class ImageCropperView {
-
-  static ImageCropperView _instance ;
+  ///A singleton instance of the [ImageCropperView] hence only one instance of
+  ///this class exists at all times.
+  static ImageCropperView _instance;
 
   ImageCropperView._internal();
 
+  /////////////////////////////////////////////////////////////////////////////
+  ///Since this package uses two different native libraries, it allows granular
+  ///control for each of the (native) libraries. This can be specified respectively
+  ///for the platform using the [AndroidUiSettings] and [IOSUiSettings] classes
+  ///to define the UI look and feel!
+  /////////////////////////////////////////////////////////////////////////////
   static AndroidUiSettings _androidUiSettings = AndroidUiSettings(
-      toolbarTitle: 'Image Cropper',
-      toolbarColor: AppColors.appBarColor,
-      toolbarWidgetColor: AppColors.pureWhite,
-      cropGridColumnCount: 3,
-      cropGridRowCount: 3,
-      cropFrameStrokeWidth: 3,
-      lockAspectRatio: true,
-      showCropGrid: true,
-      backgroundColor: AppColors.blackTextColor,
-      hideBottomControls: false,
-      );
+    toolbarTitle: 'Upload',
+    toolbarColor: AppColors.appBarColor,
+    ///The background color of toolbar for scaling and specifying the rotation
+    toolbarWidgetColor: AppColors.pureWhite,
+    cropGridColumnCount: 3,
+    cropGridRowCount: 3,
+    ///Stroke width for the overall frame for the picture
+    cropFrameStrokeWidth: 3,
+    ///[lockAspectRatio] is essential to keep the aspect ratio locked at 1:1
+    lockAspectRatio: true,
+    showCropGrid: true,
+    ///The background color for the cropper view screen
+    backgroundColor: AppColors.blackTextColor,
+    hideBottomControls: false,
+  );
 
   static IOSUiSettings _iosUiSettings = IOSUiSettings(
     minimumAspectRatio: 1.0,
+    //Prevents the user from changing the aspect ratio
     aspectRatioLockEnabled: true,
-
+    //We need to show the navigation bar to let the user pop back
+    hidesNavigationBar: false,
+    //Prevents the user from resetting the aspect ratio
+    resetAspectRatioEnabled: false,
+    //The cancel button confirms if the user
+    showCancelConfirmationDialog: false,
+    resetButtonHidden: false,
   );
 
   static ImageCropperView getInstance() {
-    if(_instance==null){
-      _instance =ImageCropperView._internal();
+    if (_instance == null) {
+      _instance = ImageCropperView._internal();
     }
     return _instance;
   }
 
   static Future<File> getSquareCroppedImage(File imageFile) async {
     File croppedFile = await ImageCropper.cropImage(
-        sourcePath: imageFile.path,
-        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
-        androidUiSettings: _androidUiSettings,
-        iosUiSettings: _iosUiSettings,
+      sourcePath: imageFile.path,
+      ///[aspectRatio] specifies the pre-defined aspect ratio (square in our case)
+      ///and we won't be allowing the user to change this in the next screen
+      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      androidUiSettings: _androidUiSettings,
+      iosUiSettings: _iosUiSettings,
     );
     return croppedFile;
   }
-
 }
