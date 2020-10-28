2c856bdb9933bc0f55fafcbc91b9eb38aed599f7
Anushka <aagrawal6789@gmail.com>
2020-10-28 13:08:34 +0530

    added validations for add item and variation , added dropdown for unit selection

--- lib/services/validation.dart
+++ lib/services/validation.dart
@@ -3,4 +3,16 @@ class ValidationService {
     if (value.isEmpty) return 'This field is required';
     return null;
   }
+
+  String validateDouble(String value) {
+    if (value.isEmpty) return 'This field is required';
+    if (double.tryParse(value) == null) return 'Input value should be a number';
+    return null;
+  }
+
+  String validateInt(String value) {
+    if (value.isEmpty) return 'This field is required';
+    if (int.tryParse(value) == null) return 'Input value should be an integer';
+    return null;
+  }
 }
