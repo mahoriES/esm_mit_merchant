90bf0f2331f7b15f495a39d804db712229708180
Parag Jyoti Nath <paragjnath@gmail.com>
2021-02-24 15:44:53 +0530

    Added Drawer to Review page

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foore/review_page/review_page.dart';
 import 'package:foore/services/sizeconfig.dart';
-import '../app_translations.dart';
-import 'app_drawer.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/foore-home';
@@ -19,17 +17,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Scaffold(
-      drawer: AppDrawer(),
-      drawerEnableOpenDragGesture: false,
-      appBar: AppBar(
-        title: Text(
-          AppTranslations.of(context).text("reviews_page_title"),
-        ),
-      ),
-      body: Center(
-        child: ReviewPage(),
-      ),
+    return Center(
+      child: ReviewPage(),
     );
   }
 }
